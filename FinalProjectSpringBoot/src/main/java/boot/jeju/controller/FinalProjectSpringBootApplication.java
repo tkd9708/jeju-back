7da64bc7086546fea2f3c2d21fb9fd5e3bb7e351
tkd9708 <tkd9708@naver.com>
2021-01-11 22:53:22 +0900

    setting

---
+++ FinalProjectSpringBoot/src/main/java/boot/jeju/controller/FinalProjectSpringBootApplication.java
@@ -0,0 +1,13 @@
+package boot.jeju.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectSpringBootApplication.class, args);
+	}
+
+}
---
+++ FinalProjectSpringBoot/src/test/java/boot/jeju/controller/FinalProjectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.jeju.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

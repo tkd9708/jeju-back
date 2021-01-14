44c3e1862b08a00ba29ef7488a50480e27a49716
bitcamp <bitcamp@DESKTOP-ADO1MJG>
2021-01-14 18:03:12 +0900

    shareboard,wishlist 커밋-오상근

--- FinalProjectSpringBoot/src/main/java/boot/jeju/data/SpotreviewDto.java
+++ FinalProjectSpringBoot/src/main/java/boot/jeju/data/SpotreviewDto.java
@@ -5,8 +5,7 @@ import org.apache.ibatis.type.Alias;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
-@Data
-@Alias("spotreview")
+@Alias("spotreview") 
 public class SpotreviewDto {
 	private String num;
 	private String contentsid;
@@ -16,5 +15,50 @@ public class SpotreviewDto {
 	private int likes;
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
 	private Timestamp writeday;
+	
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getContentsid() {
+		return contentsid;
+	}
+	public void setContentsid(String contentsid) {
+		this.contentsid = contentsid;
+	}
+	public String getMemNum() {
+		return memNum;
+	}
+	public void setMemNum(String memNum) {
+		this.memNum = memNum;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getStar() {
+		return star;
+	}
+	public void setStar(int star) {
+		this.star = star;
+	}
+	public int getLikes() {
+		return likes;
+	}
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+	public Timestamp getWriteday() {
+		return writeday;
+	}
+	public void setWriteday(Timestamp writeday) {
+		this.writeday = writeday;
+	}
+	
+	
 	
 }

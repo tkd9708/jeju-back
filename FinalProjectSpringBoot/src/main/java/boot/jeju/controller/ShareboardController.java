02754f15a82fa08b3756f84d5b97b9f4f5a67f93
ykh <yangyk7364@gmail.com>
2021-01-25 21:26:24 +0900

    /share/delete 수정

--- FinalProjectSpringBoot/src/main/java/boot/jeju/controller/ShareboardController.java
+++ FinalProjectSpringBoot/src/main/java/boot/jeju/controller/ShareboardController.java
@@ -13,18 +13,11 @@ import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import boot.jeju.data.ShareboardDto;
 import boot.jeju.mapper.ShareboardMapper;
-
-
 
 
 //@MultipartConfig
@@ -37,210 +30,206 @@ import boot.jeju.mapper.ShareboardMapper;
 @RestController
 @CrossOrigin
 public class ShareboardController {
-	@Autowired
-	ShareboardMapper mapper;
-	MultipartFile upload;
-	
-	String photoname;
-	
-	@GetMapping("/share/count")
-	 public int getTotalCount() {
-		return mapper.getTotalCount();
-	 }
-	
-	  @GetMapping("/share/list") 
-	  public List<ShareboardDto> shareList(@RequestParam int start,@RequestParam int perPage){
-		return mapper.getList(start, perPage);
-	  
-	  }
-	  
-	  @PostMapping(value = "/share/upload", consumes = {"multipart/form-data"})
-		public Map<String, String> fileUpload(@RequestParam MultipartFile uploadFile, HttpServletRequest request){
-			String uploadPath = request.getSession().getServletContext().getRealPath("/WEB-INF/photo");
-			System.out.println(uploadPath);
-			
-			// 이미지의 확장자 가져오기
-			int pos = uploadFile.getOriginalFilename().lastIndexOf("."); // 마지막 도트의 위치
-			String ext = uploadFile.getOriginalFilename().substring(pos);
-			
-			// 저장할 이미지명 변경하기
-			Date date = new Date();
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
-			photoname = "jeju" + sdf.format(date) + ext;
-			
-			upload = uploadFile;
-			
-			Map<String, String> map = new HashMap<String, String>();
-			map.put("photoname", photoname);
-			return map;
-		}
-	  
-	  @PostMapping(value = "/share/insert")
-      public void insert(
-            @RequestParam(value="num",defaultValue = "0") String num,
-             @RequestParam(value="regroup",defaultValue = "0") int regroup,
-             @RequestParam(value="restep",defaultValue = "0") int restep,
-             @RequestParam(value="relevel",defaultValue = "0") int relevel,
-             @RequestBody ShareboardDto dto,
-             HttpServletRequest request)
-          {
-          //System.out.println(dto.getAddr());
-          
-          if(photoname == null) {
-             dto.setPhoto("no");
-          }
-          else {
-             String path=request.getSession().getServletContext().getRealPath("/WEB-INF/photo");
-              System.out.println(path);
-            
-              // 저장폴더에 저장
-           try {
-              upload.transferTo(new File(path + "\\" + photoname));
-           } catch (IllegalStateException e) {
-              // TODO Auto-generated catch block
-              e.printStackTrace();
-           } catch (IOException e) {
-              // TODO Auto-generated catch block
-              e.printStackTrace();
-           }
-             
-             dto.setPhoto(photoname);
-          }
-             
-             
-          
-          if(dto.getNum() == null) {
-             regroup=mapper.getMaxNum()+1;
-             restep=0;
-             relevel=0;
-          }else {
-             mapper.updateRestep(regroup, restep);
-             
-             relevel+=1;
-             restep+=1;            
-               
-          }
-          if(dto.getRelevel()!=0) {
-             dto.setSubject("no"); 
-             dto.setAddr("no");  
-             dto.setLikes(0);
-              dto.setStar("0");
-              
-              dto.setRegroup(regroup);
-             dto.setRestep(restep);
-          }else {
-             dto.setLikes(0);
-             dto.setRegroup(regroup);
-             dto.setRelevel(relevel);
-             dto.setRestep(restep);
-          }
-          mapper.insertBoard(dto);
-          
-          photoname = null;
-          upload = null;
+    @Autowired
+    ShareboardMapper mapper;
+    MultipartFile upload;
+
+    String photoname;
+
+    @GetMapping("/share/count")
+    public int getTotalCount() {
+        return mapper.getTotalCount();
     }
-	  
 
-	  @GetMapping("/share/select")
-	  public ShareboardDto getData(@RequestParam String num) {
-		  return mapper.getData(num);
-	  }
-	  
-	  @GetMapping("/share/answer")
-	  public List<ShareboardDto> getReviewList(@RequestParam int regroup){
-		  return mapper.getReviewList(regroup);
-	  }
-	  
-	  @GetMapping("/share/updatelikes")
-	  public void updatelikes(@RequestParam String num) {
-		   mapper.updateLikes(num);
-	  }
-	  
-	 
+    @GetMapping("/share/list")
+    public List<ShareboardDto> shareList(@RequestParam int start, @RequestParam int perPage) {
+        return mapper.getList(start, perPage);
 
-	  @GetMapping("/share/delete")
-	  public void sharedelete(@RequestParam int regroup,@RequestParam String num,HttpServletRequest request) {
-		  List<ShareboardDto> deletePhotos=mapper.getPhotos(regroup);
-		  
-			  for(int i=0; i<deletePhotos.size(); i++) {
-				  String photos = deletePhotos.get(i).getPhoto();
-				  
-				  if(!photos.equals("no")) {
-				  String path=request.getSession().getServletContext().getRealPath("/WEB-INF/photo");
-				  //System.out.println(path);
-				  File file=new File(path+"\\"+photos);
-				  if(file.exists())
-					  file.delete();
-			     }
-			  } 
-			  
-			  mapper.sharedelete(num,regroup);
-		  }
-	  
-	  @GetMapping("/share/deleteanswer")
-	  public void deleteReview(@RequestParam String num,HttpServletRequest request) {
-		  String deletePhoto=mapper.getData(num).getPhoto();
-		  if(!deletePhoto.equals("no")) { 
-			  String path=request.getSession().getServletContext().getRealPath("/WEB-INF/photo"); 
-			  File file =new File(path+"\\"+deletePhoto); 
-			  if(file.exists()) 
-				  file.delete(); 
-		  } 
-		  mapper.deleteReview(num);
-	  }
-	  
-	  @GetMapping("/share/answercount")
-	  public int getCount(@RequestParam String num) {
-		  return mapper.getCount(num);
-	  }
-	  
-	  @PostMapping(value = "/share/update")
-	  public void updateShareboard(@RequestBody ShareboardDto dto, HttpServletRequest request) {
-		  
-		  if(photoname == null)
-			  dto.setPhoto(null);
-		  else {
-			//기존 이미지존재할 경우 지우기
-			  String deletePhoto=mapper.getData(dto.getNum()).getPhoto();
-			  String path=request.getSession().getServletContext().getRealPath("/WEB-INF/photo");
-			  System.out.println(path);
-			  
-			  if(!deletePhoto.equals("no")) {
-				  File file=new File(path+"\\"+deletePhoto);
-				  if(file.exists())
-					  file.delete();
-			  }
-				  
-			  try {
-				  upload.transferTo(new File(path+"\\"+photoname));
-			  } catch (IllegalStateException | IOException e) {
-				  // TODO Auto-generated catch block
-				  e.printStackTrace();
-			  }
+    }
 
-			  dto.setPhoto(photoname); 
-			  
-		  }
-		  
-		  mapper.updateShareBoard(dto);
-		  photoname = null;
-          upload = null;
-	  }
-	  
-	  @GetMapping("/share/search")
-	  public List<ShareboardDto> getsearchList(@RequestParam int start,@RequestParam int perPage,@RequestParam String search){
-		  return mapper.getSearchList(start, perPage, search);
-	  }
-	  
-	  @GetMapping("/share/searchcount")
-	  public int getsearchTotalCount(@RequestParam String search) {
-		  return mapper.getSearchTotalCount(search);
-	  }
-	  
-	  @GetMapping("/share/updateanswer")
-	  public void updateshareboardanswer(@RequestParam String content,@RequestParam String num) {
-		  mapper.updateShareBoardAnswer(content, num);
-	  }
-	  
-	  
+    @PostMapping(value = "/share/upload", consumes = {"multipart/form-data"})
+    public Map<String, String> fileUpload(@RequestParam MultipartFile uploadFile, HttpServletRequest request) {
+        String uploadPath = request.getSession().getServletContext().getRealPath("/WEB-INF/photo");
+        System.out.println(uploadPath);
+
+        // 이미지의 확장자 가져오기
+        int pos = uploadFile.getOriginalFilename().lastIndexOf("."); // 마지막 도트의 위치
+        String ext = uploadFile.getOriginalFilename().substring(pos);
+
+        // 저장할 이미지명 변경하기
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        photoname = "jeju" + sdf.format(date) + ext;
+
+        upload = uploadFile;
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("photoname", photoname);
+        return map;
+    }
+
+    @PostMapping(value = "/share/insert")
+    public void insert(
+            @RequestParam(value = "num", defaultValue = "0") String num,
+            @RequestParam(value = "regroup", defaultValue = "0") int regroup,
+            @RequestParam(value = "restep", defaultValue = "0") int restep,
+            @RequestParam(value = "relevel", defaultValue = "0") int relevel,
+            @RequestBody ShareboardDto dto,
+            HttpServletRequest request) {
+        //System.out.println(dto.getAddr());
+
+        if (photoname == null) {
+            dto.setPhoto("no");
+        } else {
+            String path = request.getSession().getServletContext().getRealPath("/WEB-INF/photo");
+            System.out.println(path);
+
+            // 저장폴더에 저장
+            try {
+                upload.transferTo(new File(path + "\\" + photoname));
+            } catch (IllegalStateException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            dto.setPhoto(photoname);
+        }
+
+
+        if (dto.getNum() == null) {
+            regroup = mapper.getMaxNum() + 1;
+            restep = 0;
+            relevel = 0;
+        } else {
+            mapper.updateRestep(regroup, restep);
+
+            relevel += 1;
+            restep += 1;
+
+        }
+        if (dto.getRelevel() != 0) {
+            dto.setSubject("no");
+            dto.setAddr("no");
+            dto.setLikes(0);
+            dto.setStar("0");
+
+            dto.setRegroup(regroup);
+            dto.setRestep(restep);
+        } else {
+            dto.setLikes(0);
+            dto.setRegroup(regroup);
+            dto.setRelevel(relevel);
+            dto.setRestep(restep);
+        }
+        mapper.insertBoard(dto);
+
+        photoname = null;
+        upload = null;
+    }
+
+
+    @GetMapping("/share/select")
+    public ShareboardDto getData(@RequestParam String num) {
+        return mapper.getData(num);
+    }
+
+    @GetMapping("/share/answer")
+    public List<ShareboardDto> getReviewList(@RequestParam int regroup) {
+        return mapper.getReviewList(regroup);
+    }
+
+    @GetMapping("/share/updatelikes")
+    public void updatelikes(@RequestParam String num) {
+        mapper.updateLikes(num);
+    }
+
+
+    @PostMapping("/share/delete")
+    public void sharedelete(@RequestBody ShareboardDto dto, HttpServletRequest request) {
+        List<ShareboardDto> deletePhotos = mapper.getPhotos(dto.getRegroup());
+
+        for (int i = 0; i < deletePhotos.size(); i++) {
+            String photos = deletePhotos.get(i).getPhoto();
+
+            if (!photos.equals("no")) {
+                String path = request.getSession().getServletContext().getRealPath("/WEB-INF/photo");
+                //System.out.println(path);
+                File file = new File(path + "\\" + photos);
+                if (file.exists())
+                    file.delete();
+            }
+        }
+
+        mapper.sharedelete(dto.getNum(), dto.getRegroup());
+    }
+
+    @GetMapping("/share/deleteanswer")
+    public void deleteReview(@RequestParam String num, HttpServletRequest request) {
+        String deletePhoto = mapper.getData(num).getPhoto();
+        if (!deletePhoto.equals("no")) {
+            String path = request.getSession().getServletContext().getRealPath("/WEB-INF/photo");
+            File file = new File(path + "\\" + deletePhoto);
+            if (file.exists())
+                file.delete();
+        }
+        mapper.deleteReview(num);
+    }
+
+    @GetMapping("/share/answercount")
+    public int getCount(@RequestParam String num) {
+        return mapper.getCount(num);
+    }
+
+    @PostMapping(value = "/share/update")
+    public void updateShareboard(@RequestBody ShareboardDto dto, HttpServletRequest request) {
+
+        if (photoname == null)
+            dto.setPhoto(null);
+        else {
+            //기존 이미지존재할 경우 지우기
+            String deletePhoto = mapper.getData(dto.getNum()).getPhoto();
+            String path = request.getSession().getServletContext().getRealPath("/WEB-INF/photo");
+            System.out.println(path);
+
+            if (!deletePhoto.equals("no")) {
+                File file = new File(path + "\\" + deletePhoto);
+                if (file.exists())
+                    file.delete();
+            }
+
+            try {
+                upload.transferTo(new File(path + "\\" + photoname));
+            } catch (IllegalStateException | IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            dto.setPhoto(photoname);
+
+        }
+
+        mapper.updateShareBoard(dto);
+        photoname = null;
+        upload = null;
+    }
+
+    @GetMapping("/share/search")
+    public List<ShareboardDto> getsearchList(@RequestParam int start, @RequestParam int perPage, @RequestParam String search) {
+        return mapper.getSearchList(start, perPage, search);
+    }
+
+    @GetMapping("/share/searchcount")
+    public int getsearchTotalCount(@RequestParam String search) {
+        return mapper.getSearchTotalCount(search);
+    }
+
+    @GetMapping("/share/updateanswer")
+    public void updateshareboardanswer(@RequestParam String content, @RequestParam String num) {
+        mapper.updateShareBoardAnswer(content, num);
+    }
+
+
 }

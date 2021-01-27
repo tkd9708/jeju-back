20769d6ba22cdde2227780e8fa7d1bac823e7783
sanggeun-oh <rjfjqj03@naver.com>
2021-01-27 14:19:53 +0900

    컨트롤러 수정

--- FinalProjectSpringBoot/src/main/java/boot/jeju/controller/WishlistController.java
+++ FinalProjectSpringBoot/src/main/java/boot/jeju/controller/WishlistController.java
@@ -76,7 +76,7 @@ public class WishlistController {
 	}
 	
 	@GetMapping("/wish/spottitle")
-	public String getSpottitle(@RequestParam String contentsid) {
+	public String getSpottitle(@RequestParam(value="contentsid",defaultValue = "0") String contentsid) {
 		return mapper.getSpottitle(contentsid);
 	}
 	@GetMapping("/wish/count")
@@ -84,8 +84,9 @@ public class WishlistController {
 		return mapper.getTotalCount(memNum);
 	}
 	@GetMapping("/wish/sharesubject")
-	public String getsharesubject(@RequestParam String num) {
+	public String getsharesubject(@RequestParam(value="num",defaultValue = "0") String num ) {
 		return mapper.getShareSubject(num);
+		
 	}
 	@GetMapping("/wish/wishcount")
 	public int getWishtotalCount(@RequestParam String memId,@RequestParam String category) {

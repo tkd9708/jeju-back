568296fae68f9388ac4432d8ff096ca8cb7c3355
redhorn64 <reliable0524@gmail.com>
2021-02-04 09:52:22 +0900

    rentcar 관련 부분 수정

--- FinalProjectSpringBoot/src/main/java/boot/jeju/controller/ReservationController.java
+++ FinalProjectSpringBoot/src/main/java/boot/jeju/controller/ReservationController.java
@@ -3,16 +3,20 @@ package boot.jeju.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import boot.jeju.data.RentCarDto;
 import boot.jeju.mapper.ReservationMapper;
 
+@RestController
+@CrossOrigin
 public class ReservationController {
 	@Autowired
 	ReservationMapper mapper;
 	
-	@PostMapping("/reservation/carlist")
+	@GetMapping("/reservation/carlist")
 	public List<RentCarDto> getCarlist(){
 		return mapper.getListOfRentCar();
 	}

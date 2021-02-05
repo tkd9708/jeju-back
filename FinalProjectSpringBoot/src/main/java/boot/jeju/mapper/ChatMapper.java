2f7dec689d04b45b6452246e84689ab53a4c825b
tkd9708 <tkd9708@naver.com>
2021-02-05 11:26:46 +0900

    채팅 searchId 추가

--- FinalProjectSpringBoot/src/main/java/boot/jeju/mapper/ChatMapper.java
+++ FinalProjectSpringBoot/src/main/java/boot/jeju/mapper/ChatMapper.java
@@ -3,6 +3,8 @@ package boot.jeju.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import boot.jeju.data.ChatmsgDto;
 import boot.jeju.data.ChatroomDto;
@@ -11,6 +13,7 @@ import boot.jeju.data.ChatroomDto;
 public interface ChatMapper {
 	public void createRoom(ChatroomDto dto);
 	public List<ChatroomDto> getRoom(String user);
+	public List<ChatroomDto> getSearchIdRoom(@Param("user") String user, @Param("searchId") String searchId);
 	public void insertMsg(ChatmsgDto dto);
 	public List<ChatmsgDto> getMsgs(String roomNum);
 }

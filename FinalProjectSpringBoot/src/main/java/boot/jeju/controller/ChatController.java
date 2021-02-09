d7801fc4f374a45d5fad926c0d9b325cf881250f
ykh <yangyk7364@gmail.com>
2021-02-09 16:07:33 +0900

    [chatting] modified api.

--- FinalProjectSpringBoot/src/main/java/boot/jeju/controller/ChatController.java
+++ FinalProjectSpringBoot/src/main/java/boot/jeju/controller/ChatController.java
@@ -22,47 +22,54 @@ public class ChatController {
 
 	@Autowired
 	ChatMapper mapper;
-	
+
 	// 방 생성
 	@PostMapping("/chat/createRoom")
 	public void createRoom(@RequestBody ChatroomDto dto){
 		mapper.createRoom(dto);
 	}
-	
+
 	// user가 속한 방 리스트
 	@GetMapping("/chat/getRoom")
 	public List<ChatroomDto> getRoom(@RequestParam String user){
-		return mapper.getRoom(user);
+//		return mapper.getRoom(user);
+		List<ChatroomDto> list = mapper.getRoom(user);
+		for(ChatroomDto dto : list){
+			String lastMsg = mapper.getLastMsg(dto.getNum()).get(0).getMsg();
+			dto.setLastMsg(lastMsg);
+		}
+
+		return list;
 	}
-	
-	// user이 searchId 검색 
+
+	// user이 searchId 검색
 	@GetMapping("/chat/getSearchIdRoom")
 	public List<ChatroomDto> getSearchIdRoom(@RequestParam String user, @RequestParam String searchId){
 		return mapper.getSearchIdRoom(user, searchId);
 	}
-	
+
 	// 해당 방의 메시지 리스트
 	@GetMapping("/chat/getMsgs")
 	public List<ChatmsgDto> getMsgs(@RequestParam String roomNum){
 		return mapper.getMsgs(roomNum);
 	}
-	
+
 	// 메시지 입력
 	@PostMapping("/chat/insertMsg")
 	public void insertMsg(@RequestBody ChatmsgDto dto) {
 		mapper.insertMsg(dto);
 	}
-	
+
 	// 각 방마다 마지막 메시지
 	@GetMapping("/chat/lastMsg")
 	public Map<String, String> getLastMsg(@RequestParam String roomNum){
 		String lastMsg = mapper.getLastMsg(roomNum).get(0).getMsg();
-		
+
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("lastMsg", lastMsg);
 		return map;
 	}
-	
+
 	// 각 방의 메시지 갯수
 	@GetMapping("/chat/msgCount")
 	public int getMsgTotalCount(@RequestParam String roomNum) {
